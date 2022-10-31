b8de410f89e99470ea1f101195ed56d35f86428e
fransan990 <fransan990@gmail.com>
2022-10-31 09:06:56 +0100

    projects-config

---
+++ src/config/projects-config.js
@@ -0,0 +1,36 @@
+const projects = [
+    {
+        id: 1,
+        imagen: "../../images/imagenProyecto1.png",
+        nombre: "Juego",
+        categoria: "todo",
+        tecnologias: "| HTML5 | CSS3 | JavaScript | Canvas js",
+        descripcion: "Clásico juego de supervivencia de zombies desarrollado con Canvas y manipulación de DOM donde se usa mucho diseño."
+
+
+
+    },
+    {
+        id: 2,
+        imagen: "../../images/imagenProyecto2.png",
+        nombre: "Recipe",
+        categoria: "principal",
+        tecnologias: "| HTML5 | CSS3 | JavaScript | Express.js | Node.js | MongoDB | GOOGLE API | HBS | Bootstrap v5.0 | Mongoose |",
+        descripcion: "Aplicación web en la que los usuarios pueden ver las recetas creadas por ellos mismos pudiendo editar y eliminar sus propias recetas tambien pueden buscar cualquier otra receta que les interese y guardar esa receta donde luego la podran ver en su perfil ademas se utiliza la API de Google Places para que puedan encontrar el nombre y la ubicación de los restaurantes donde se encuentra esa receta."
+
+
+
+    },
+    {
+        id: 3,
+        imagen: "../../images/imagenProyecto2.png",
+        nombre: "gelioX",
+        categoria: "principal",
+        tecnologias: "| React | React-bootstrap | Aos | HTML5 | CSS3 | Javascript | Express | Node.js | MongoDB | Mongoose | JWS |",
+        descripcion: "Aplicación web en la que los usuarios pueden ver las recetas creadas por ellos mismos pudiendo editar y eliminar sus propias recetas tambien pueden buscar cualquier otra receta que les interese y guardar esa receta donde luego la podran ver en su perfil ademas se utiliza la API de Google Places para que puedan encontrar el nombre y la ubicación de los restaurantes donde se encuentra esa receta."
+
+
+
+    }
+]
+export default projects
