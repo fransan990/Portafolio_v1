51587e906a7bb35c0108b59b1af15a9ce2151bf6
fransan990 <fransan990@gmail.com>
2022-11-07 22:34:09 +0100

    projects.js

--- src/config/projects-config.js
+++ src/config/projects-config.js
@@ -16,18 +16,18 @@ const projects = [
         nombre: "Recipe",
         categoria: "principal",
         tecnologias: "| HTML5 | CSS3 | JavaScript | Express.js | Node.js | MongoDB | GOOGLE API | HBS | Bootstrap v5.0 | Mongoose |",
-        descripcion: "Aplicación web en la que los usuarios pueden ver las recetas creadas por ellos mismos pudiendo editar y eliminar sus propias recetas tambien pueden buscar cualquier otra receta que les interese y guardar esa receta donde luego la podran ver en su perfil ademas se utiliza la API de Google Places para que puedan encontrar el nombre y la ubicación de los restaurantes donde se encuentra esa receta."
+        descripcion: "Aplicación web realizada en Back-End en la que los usuarios pueden ver las recetas creadas por ellos mismos pudiendo editar y eliminar sus propias recetas tambien pueden buscar cualquier otra receta que les interese y guardar esa receta donde luego la podran ver en su perfil ademas se utiliza la API de Google Places para que puedan encontrar el nombre y la ubicación de los restaurantes donde se encuentra esa receta."
 
 
 
     },
     {
         id: 3,
-        imagen: "../../images/imagenProyecto2.png",
+        imagen: "https://github.com/fransan990/gelioX_Client/blob/main/public/images/InicioPagina.png",
         nombre: "gelioX",
         categoria: "principal",
         tecnologias: "| React | React-bootstrap | Aos | HTML5 | CSS3 | Javascript | Express | Node.js | MongoDB | Mongoose | JWS |",
-        descripcion: "Aplicación web en la que los usuarios pueden ver las recetas creadas por ellos mismos pudiendo editar y eliminar sus propias recetas tambien pueden buscar cualquier otra receta que les interese y guardar esa receta donde luego la podran ver en su perfil ademas se utiliza la API de Google Places para que puedan encontrar el nombre y la ubicación de los restaurantes donde se encuentra esa receta."
+        descripcion: "Aplicación web full stack por la parte de Back-End con sus modelos conexiones a la base de datos de MongoDB y sus rutas y por la parte de Front-End realizada con React "
 
 
 
